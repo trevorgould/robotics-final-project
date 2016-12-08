723bf65119a345ed570f6796ae037f4f6547e08f
Aycel Villalobos <aycelvillalobos@rl1-194-4-dhcp.int.colorado.edu>
2016-12-08 15:43:28 -0700

    Removed sprki.print() statements

--- sparki/sparki.ino
+++ sparki/sparki.ino
@@ -274,34 +274,34 @@ int reactiveBehaviors() {
       sparki.moveRight(180);
       state = FOLLOW;
       break;
+// Line Following
+//     case FOLLOW: {
+//       sparki.moveForward();
+//       int threshold = 500;
+//       int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
+//       int lineCenter = sparki.lineCenter(); // measure the center IR sensor
+//       int lineRight  = sparki.lineRight();  // measure the right IR sensor
 
-    case FOLLOW: {
-      sparki.moveForward();
-      int threshold = 500;
-      int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
-      int lineCenter = sparki.lineCenter(); // measure the center IR sensor
-      int lineRight  = sparki.lineRight();  // measure the right IR sensor
+//       if ( lineLeft < threshold ) sparki.moveLeft();
+//       if ( lineRight < threshold ) sparki.moveRight();
+//       if ( ((lineLeft + lineRight + lineCenter) / 3) < 195 ) state = DROP;
 
-      if ( lineLeft < threshold ) sparki.moveLeft();
-      if ( lineRight < threshold ) sparki.moveRight();
-      if ( ((lineLeft + lineRight + lineCenter) / 3) < 195 ) state = DROP;
-
-      // if the center line sensor is the only one reading a line
-      if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) ) {
-        sparki.moveForward();
-      }
+//       // if the center line sensor is the only one reading a line
+//       if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) ) {
+//         sparki.moveForward();
+//       }
 
       sparki.clearLCD();
-      sparki.print("Line Left: "); // show left line sensor on screen
+      
       sparki.println(lineLeft);
 
-      sparki.print("Line Center: "); // show center line sensor on screen
+      
       sparki.println(lineCenter);
 
-      sparki.print("Line Right: "); // show right line sensor on screen
+      
       sparki.println(lineRight);
 
-      sparki.print("AVG: ");
+      
       sparki.println((lineLeft + lineRight + lineCenter) / 3);
 
       sparki.updateLCD(); // display all of the information written to the screen
